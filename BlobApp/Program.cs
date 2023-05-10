aae1ea04f753e4517cd4fc723c55e969dbdc0456
Quang Duy <justinquangduy@gmail.com>
2023-05-10 15:59:13 +0700

    Downloading a Blob

--- BlobApp/Program.cs
+++ BlobApp/Program.cs
@@ -4,7 +4,7 @@ using Azure.Storage.Blobs.Models;
 string connectionString = "DefaultEndpointsProtocol=https;AccountName=jtappstore;AccountKey=hTeY0UshDRhr+vvDDtvnyaB3mp24zGKyas2G8s+MbCGvrIzgCRUhrrjWDKA1YYafmjk2+K5IHp1D+AStAim0Hg==;EndpointSuffix=core.windows.net";
 string containerName = "data";
 string blobName = "script.sql";
-string filePath = "D:\\Study";
+string filePath = "D:\\Study\\script.sql";
 
 BlobClient blobClient =new BlobClient(connectionString, containerName,blobName);
 

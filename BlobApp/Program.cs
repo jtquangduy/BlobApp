65a956cf0fec9d0c40c193e5aae6d7131fde58da
Quang Duy <justinquangduy@gmail.com>
2023-05-10 15:08:40 +0700

    Create a container and access type level

--- BlobApp/Program.cs
+++ BlobApp/Program.cs
@@ -1,12 +1,12 @@
 ﻿using Azure.Storage.Blobs;
-
+using Azure.Storage.Blobs.Models;
 string connectionString = "DefaultEndpointsProtocol=https;AccountName=jtappstore;AccountKey=hTeY0UshDRhr+vvDDtvnyaB3mp24zGKyas2G8s+MbCGvrIzgCRUhrrjWDKA1YYafmjk2+K5IHp1D+AStAim0Hg==;EndpointSuffix=core.windows.net";
-string containerName = "scripts";
+string containerName = "scripts1";
 
 BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
 
 Console.WriteLine("Creating the container");
 
-await blobServiceClient.CreateBlobContainerAsync(containerName);
+await blobServiceClient.CreateBlobContainerAsync(containerName,PublicAccessType.Blob);
 
 Console.WriteLine("Container creation complete");

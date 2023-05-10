b5c0e312f43d4063333f0e55dda94f8fa5385433
Quang Duy <justinquangduy@gmail.com>
2023-05-10 16:08:57 +0700

    Blob Metadata

--- BlobApp/Program.cs
+++ BlobApp/Program.cs
@@ -3,11 +3,32 @@ using Azure.Storage.Blobs.Models;
 
 string connectionString = "DefaultEndpointsProtocol=https;AccountName=jtappstore;AccountKey=hTeY0UshDRhr+vvDDtvnyaB3mp24zGKyas2G8s+MbCGvrIzgCRUhrrjWDKA1YYafmjk2+K5IHp1D+AStAim0Hg==;EndpointSuffix=core.windows.net";
 string containerName = "data";
-string blobName = "script.sql";
-string filePath = "D:\\Study\\script.sql";
 
-BlobClient blobClient =new BlobClient(connectionString, containerName,blobName);
+await GetMetaData();
 
-await blobClient.DownloadToAsync(filePath);
+async Task SetBlobMetaData()
+{
+    string blobName = "script.sql";
 
-Console.WriteLine("The blob is downloaded");
+    BlobClient blobClient = new BlobClient(connectionString,containerName, blobName);
+
+    IDictionary<string,string> metaData = new Dictionary<string,string>();
+    metaData.Add("Department", "Logistics");
+    metaData.Add("Application", "AppA");
+
+    await blobClient.SetMetadataAsync(metaData);
+
+    Console.WriteLine("Metadata added");
+}
+
+async Task GetMetaData()
+{
+    string blobName = "script.sql";
+    BlobClient blobClient = new BlobClient(connectionString, containerName, blobName);
+    BlobProperties blobProperties = await blobClient.GetPropertiesAsync();
+    foreach (var metaData in blobProperties.Metadata)
+    {
+        Console.WriteLine("The key is {0}", metaData.Key);
+        Console.WriteLine("The value is {0}", metaData.Value);
+    }
+}

02f9e105f659f8f6e7a235f2862a381c4acdc927
Quang Duy <justinquangduy@gmail.com>
2023-05-10 15:15:09 +0700

    Uploading a Blob

--- BlobApp/Program.cs
+++ BlobApp/Program.cs
@@ -1,12 +1,22 @@
 ﻿using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Models;
+//using Azure.Storage.Blobs.Models;
+
+
 string connectionString = "DefaultEndpointsProtocol=https;AccountName=jtappstore;AccountKey=hTeY0UshDRhr+vvDDtvnyaB3mp24zGKyas2G8s+MbCGvrIzgCRUhrrjWDKA1YYafmjk2+K5IHp1D+AStAim0Hg==;EndpointSuffix=core.windows.net";
-string containerName = "scripts1";
+string containerName = "scripts";
+string blobName = "script.sql";
+string filepath = "D:\\Study\\mysql\\script.sql";
 
-BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+//BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
 
-Console.WriteLine("Creating the container");
+BlobContainerClient blobServiceClient = new BlobContainerClient(connectionString,containerName);
 
-await blobServiceClient.CreateBlobContainerAsync(containerName,PublicAccessType.Blob);
+BlobClient blobClient = blobServiceClient.GetBlobClient(blobName);
+await blobClient.UploadAsync(filepath, true);
+Console.WriteLine("Upload the blob");
 
-Console.WriteLine("Container creation complete");
+//Console.WriteLine("Creating the container");
+
+//await blobServiceClient.CreateBlobContainerAsync(containerName,PublicAccessType.Blob);
+
+//Console.WriteLine("Container creation complete");

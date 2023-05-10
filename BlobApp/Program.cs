cf50de24ae2d9e958cc27a21797ab783c7b91210
Quang Duy <justinquangduy@gmail.com>
2023-05-10 15:06:10 +0700

    Add project files.

---
+++ BlobApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Azure.Storage.Blobs;
+
+string connectionString = "DefaultEndpointsProtocol=https;AccountName=jtappstore;AccountKey=hTeY0UshDRhr+vvDDtvnyaB3mp24zGKyas2G8s+MbCGvrIzgCRUhrrjWDKA1YYafmjk2+K5IHp1D+AStAim0Hg==;EndpointSuffix=core.windows.net";
+string containerName = "scripts";
+
+BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+Console.WriteLine("Creating the container");
+
+await blobServiceClient.CreateBlobContainerAsync(containerName);
+
+Console.WriteLine("Container creation complete");
